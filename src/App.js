4a2959de2c81267271596346531d814aa4f01c5a
davquipe <davidcondori99@gmail.com>
2022-08-04 22:38:23 -0500

    Let's add a loader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { Col } from 'antd';
+import { Col, Spin } from 'antd';
 import PokemonList from './components/PokemonList';
 import Searcher from './components/Searche';
 import './App.css';
 import logo from './static/logo.svg'
 import { useEffect } from 'react';
 import { getPokemon } from './api';
-import { getPokemonsWithDetails } from './actions';
+import { getPokemonsWithDetails, setLoading } from './actions';
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
 
-  const pokemons = useSelector(state => state.pokemons);
+  const pokemons = useSelector((state) => state.pokemons);
+  const loading = useSelector((state) => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      dispatch(setLoading(true));
       const pokemonsRes = await getPokemon();
       dispatch(getPokemonsWithDetails(pokemonsRes));
+      dispatch(setLoading(false))
     };
 
     fetchPokemons();
@@ -31,7 +34,12 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+      {loading ? (<Col offset={12}>
+        <Spin spinning size='large' />
+      </Col>
+      ) : (
+        <PokemonList pokemons={pokemons} />
+      )}
     </div>
   );
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 // export it as constant
 
-export const SET_POKEMONS = 'SET_POKEMONS'
+export const SET_POKEMONS = 'SET_POKEMONS';
+export const SET_LOADING = 'SET_LOADING';
