166b623f2cd71e5581ad96d1e58acb30d70974ce
davquipe <davidcondori99@gmail.com>
2022-08-05 19:03:46 -0500

    deploy to Github Pages

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,13 @@ export const getPokemon = () => {
         .catch((err) => console.log(err))
 }
 
+export const getPokemons = (name) => {
+    return axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((res) => res.data)
+        .catch((err) => err);
+}
+
 export const getPokemonDetails = (pokemon) => {
     return axios.get(pokemon.url)
         .then(res => res.data)
